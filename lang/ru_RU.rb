e0c2e896563d363bf06d508576dadd4620912e6e
Mikhail Shirkov <msh@cherry.(none)>
2009-03-18 09:12:51 +0300

    Another step to new design

---
+++ lang/ru_RU.rb
@@ -0,0 +1,11 @@
+Localization.define("ru_RU") do |l|
+
+  l.store "Continue reading...", "Читать дальше..."
+  l.store "Leave a comment", "Комментировать"
+  l.store "Pages", "Странички"
+  l.store "Categories", "Разделы"
+  l.store "Archives", "Архив"
+  l.store "Posted in", "В разделе"
+  l.store "Tags", "Метки"
+  l.store "Comments", "Комментарии"
+end
